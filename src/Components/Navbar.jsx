3f05537b170fe2154dd872fd78543393cc29d062
SebaPrado <sebastian,pradomelesi@gmail.com>
2024-08-26 11:07:01 +0530

    Agregando archivos del proyecto, incluyendo README local

---
+++ src/Components/Navbar.jsx
@@ -0,0 +1,77 @@
+import React, { useState } from 'react';
+import '../index.css';
+import { AppBar, Toolbar, IconButton, Button, Drawer, List, ListItem, ListItemText, Box } from '@mui/material';
+import MenuIcon from '@mui/icons-material/Menu';
+import { createTheme, ThemeProvider } from '@mui/material/styles';
+import resumePDF from '../../public/Cv Sebastian Prado.pdf';
+
+const theme = createTheme({
+  typography: {
+    fontFamily: 'Montserrat, sans-serif',
+  },
+});
+
+function Navbar() {
+  const [drawerOpen, setDrawerOpen] = useState(false);
+
+  const toggleDrawer = (open) => (event) => {
+    if (event.type === 'keydown' && (event.key === 'Tab' || event.key === 'Shift')) {
+      return;
+    }
+    setDrawerOpen(open);
+  };
+
+  const handleResumeClick = () => {
+    window.open(resumePDF, '_blank');
+  };
+
+  const menuItems = [
+    { label: 'Home', onClick: () => {} },
+    { label: 'Projects', onClick: () => {} },
+    { label: 'About', onClick: () => {} },
+    { label: 'Resume', onClick: handleResumeClick },
+  ];
+
+  return (
+    <>  
+     <div className='container'>    
+      <ThemeProvider theme={theme}>
+      <Box className="main-container">
+        <AppBar position="static" className="custom-app-bar" sx={{ backgroundColor: 'black' }}>
+          <Toolbar className="custom-toolbar">
+            <IconButton
+              edge="start"
+              color="inherit"
+              aria-label="menu"
+              sx={{ mr: 2, display: { sm: 'none' }, color: 'white' }}
+              onClick={toggleDrawer(true)}
+            >
+              <MenuIcon />
+            </IconButton>
+            <Box className="button-container">
+              {menuItems.map((item) => (
+                <Button key={item.label} className="custom-button" sx={{ color: 'white' }} onClick={item.onClick}>
+                  {item.label}
+                </Button>
+              ))}
+            </Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Drawer anchor="left" open={drawerOpen} onClose={toggleDrawer(false)}>
+        <List>
+          {menuItems.map((item) => (
+            <ListItem button key={item.label} onClick={toggleDrawer(false)}>
+              <ListItemText primary={item.label} />
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+    </ThemeProvider>
+    </div>
+    </>
+
+  );
+}
+
+export default Navbar;
---
+++ src/Components/views/About.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+
+function About() {
+  return (
+    <>
+    <div>About</div>
+     <div className='flexbox-container'>
+      <div className='flexbox-item'>con flex-wrap: wrap</div>
+      <div className='flexbox-item flexbox-item1 '> flexbox1</div>
+      <div className='flexbox-item flexbox-item2'> flexbox2</div>
+      <div className='flexbox-item flexbox-item3'>flexbox3</div>
+
+     </div>
+     <div className='flexbox-container2'>
+      <div className='flexbox-item flexbox-item4 '> flexbox4 tiene un flex-shrink: 0</div>
+      <div className='flexbox-item flexbox-item5'> flexbox5 flex-shrink:2</div>
+      <div className='flexbox-item flexbox-item6'>flexbox6 tiene un flex-grow: 1</div>
+
+     </div>
+     <div className='flexbox-container3'>
+      <div className='flexbox-item flexbox-item7 '> flexbox 7</div>
+      <div className='flexbox-item flexbox-item8'> flexbox 8 </div>
+      <div className='flexbox-item flexbox-item9'>flexbox 9 </div>
+
+     </div>
+    </>
+  )
+}
+
+export default About
